637301c509a2a7ac2ef5139109132501f6a4d68f
E8LL4IQuU <ayu@danwin1210.de>
2023-11-07 06:20:11 +0100

    fix type error

--- ano/src/components/Connect.vue
+++ ano/src/components/Connect.vue
@@ -31,7 +31,7 @@
       <iframe
           class="m-auto pc:w-[700px] pc:h-[500px] tablet:w-[400px] tablet:h-[300px] mobile:w-[300px] mobile:h-[200px]"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2225.6905459132345!2d54.240400900000004!3d56.0932713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43e0a1bbf6bb80dd%3A0x6a708bb0426d7de7!2z0K7QsdC40LvQtdC50L3Ri9C5INC_0YAuLCAxMywg0J3QtdGE0YLQtdC60LDQvNGB0LosINCg0LXRgdC_LiDQkdCw0YjQutC-0YDRgtC-0YHRgtCw0L0sIDQ1MjY4Mw!5e0!3m2!1sru!2sru!4v1698818299611!5m2!1sru!2sru"
-          width="1000" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+          width="1000" height="600" style="border:0;" loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
