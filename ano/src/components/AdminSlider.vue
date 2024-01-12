c28eee1380e157467bd2219bde7d8296d2e0c898
E8LL4IQuU <ayu@danwin1210.de>
2024-01-12 05:01:29 -0500

    add slider to admin, remove unnecessary env var

---
+++ ano/src/components/AdminSlider.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h3 class="text-6xl text-center pt-4 pb-3 text-gray-500 font-medium mobile:text-4xl">Мероприятия</h3>
+    <div class="w-[100%] h-[100%]">
+      <Swiper
+          :spaceBetween="30"
+          :centeredSlides="true"
+          :autoplay="{
+      delay: 2500,
+      disableOnInteraction: false,
+    }" :navigation="true"
+          :modules="modules"
+      >
+        <SwiperSlide v-for="(slide, index) in SliderData" :key="index">
+          <div class="flex flex-col justify-center">
+            <div class="w-[100%] text-white absolute text-center justify-center top-[36%]">
+              <h2 class="text-5xl pb-3 mobile:text-3xl">{{ slide.title }}</h2>
+              <p class="text-2xl pb-3 mobile:text-sm">{{ slide.description }}</p>
+              <a class="p-2 rounded-[4px] bg-gray-400 hover:bg-gray-600 duration-300" :href="slide.links">Подробнее</a>
+            </div>
+            <img class="w-auto h-[100vh]  mobile:h-72" :src="`/uploads/${slide.img}`" alt="slider image"/>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {Swiper, SwiperSlide} from "swiper/vue";
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+interface ISlider {
+  title: string;
+  description: string;
+  links: string;
+  img: string
+}
+
+const props = defineProps<{
+  SliderData: ISlider[]
+}>()
+const modules = [
+  Autoplay,
+  Pagination,
+  Navigation
+]
+</script>
+
+<style scoped lang="sass">
+
+</style>
