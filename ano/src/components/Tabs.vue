2d302ac1e431a139243d9a8c9eefbb11a0957e5e
1Dmitry2 <pain2284@gmail.com>
2024-02-10 18:01:02 +0500

    add:add page directions

---
+++ ano/src/components/Tabs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tabs-br  flex justify-between items-center bg-[#E4E4E4] text-center tablet:block tablet:bg-transparent">
+    <input name="tab-btn" type="radio" value="tab-br1" id="tab-br1" v-model="selectedTab"/>
+    <label class="text-[24px] mac:text-[18px] pc:text-[14px] p-[10px] tablet:p-[4px] tablet:m-[5px] tablet:text-[14px]" for="tab-br1">Физкультурно-оздоровительные
+      <br class="tablet:hidden">
+      услуги</label>
+    <input name="tab-btn" type="radio" value="tab-br2" id="tab-br2" v-model="selectedTab"/>
+    <label class="text-[24px] mac:text-[18px] pc:text-[14px] p-[10px] tablet:p-[4px] tablet:m-[5px] tablet:text-[14px]" for="tab-br2">Туристические и спортивные <br class="tablet:hidden">
+      мероприятия</label>
+    <input name="tab-btn" type="radio" value="tab-br3" id="tab-br3" v-model="selectedTab"/>
+    <label class="text-[24px] mac:text-[18px] pc:text-[14px] p-[10px] tablet:p-[4px] tablet:m-[5px] tablet:text-[14px]" for="tab-br3">Организация <br class="tablet:hidden">
+      походов и сплавов </label>
+    <input name="tab-btn" type="radio" value="tab-br4" id="tab-br4" v-model="selectedTab"/>
+    <label class="text-[24px] mac:text-[18px] pc:text-[14px] p-[10px] tablet:p-[4px] tablet:m-[5px] tablet:text-[14px]" for="tab-br4">Поддержка <br class="tablet:hidden">
+      ветеранов спорта</label>
+    <input name="tab-btn" type="radio" value="tab-br5" id="tab-br5" v-model="selectedTab"/>
+    <label class="text-[24px] mac:text-[18px] pc:text-[14px] p-[10px] tablet:p-[4px] tablet:m-[5px] tablet:text-[14px]" for="tab-br5">Спортивный <br class="tablet:hidden">
+      клуб “Наш ковчег”</label>
+  </div>
+  <div class="tab-br" :id="selectedTab + '-content'">
+    <slot :name="selectedTab"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const selectedTab = ref('tab-br1');
+</script>
+
+
+<style scoped>
+
+.tabs-br input{
+  display: none;
+}
+.tabs-br label{
+  cursor: pointer;
+  color: black;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+}
+.tabs-br input:checked + label {
+  background-color: #333;
+  color: white;
+}
+</style>
--- ano/src/views/SectionsView.vue
+++ ano/src/views/SectionsView.vue
@@ -1,11 +1,99 @@
 <script setup lang="ts">
-
+import {ref} from "vue";
+import Tabs from "@/components/Tabs.vue";
+const selectedTab = ref('tab-br1')
 </script>
 
 <template>
-    <h2>Sections view</h2>
+  <div>
+  <div class="max-w-[1200px] m-auto">
+    <div>
+      <h1 class="text-black text-[40px] font-light pb-[24px]">Направления</h1>
+    </div>
+  </div>
+  <div>
+    <tabs v-model="selectedTab">
+      <template #tab-br1>
+        <p class="text-black text-[24px] max-w-[1430px] text-center font-light py-[15px] mx-auto">Более 100 детей и подростков воспользовались физкультурно-оздоровительной услугой, посещая тренировочные занятия по детской лёгкой атлетике, волейболу, баскетболу, общей физической подготовке, подготовке к сдаче норм ГТО.В 2023 году запустили новые направления для людей старше 55 лет: «Йога + суставная гимнастика», ОФП, «Шахматы», «Шашки»</p>
+        <div class="flex justify-center gap-[84px] max-w-[1200px] m-auto pc:block pc: w-full pc: mx-auto">
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-one.jpeg" alt="">
+          </div>
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-one2.jpeg" alt="">
+          </div>
+        </div>
+        <div class="py-[30px] text-center">
+        <router-link to="/gallery" class="text-white bg-black pt-[13px] pb-[13px] pr-[20px] pl-[20px] rounded-[10px]  hover:bg-[#474747] duration-300 cursor-pointer">
+          Смотреть еще фото
+        </router-link>
+        </div>
+      </template>
+      <template #tab-br2>
+        <p class="text-black text-[24px] max-w-[1430px] text-center font-light py-[15px] mx-auto">АНО «Нефтекамск спортивный» является одной из немногих организаций, проводящих физкультурно-оздоровительные и туристские мероприятия для коллективов организаций и предприятий Нефтекамска.
+          <br>
+          <br>
+          Среди наших мероприятий: зимние и летние Дни здоровья, туристические мероприятия, фестивали и слеты, спартакиады по видам спорта, квесты, физкультурно-оздоровительные мероприятия для школьников и дошкольников.
+          <br>
+          <br>
+          Среди наших клиентов: ООО «Центр отопления», ЦГБ г. Нефтекамск, АО «Белкамнефть», АО «Транснефть Урал», ПАО «Сбербанк-Нефтекамск», КГРЭС, БашРТС-Нефтекамск, УЖКХ-Нефтекамск.</p>
+        <div class="flex justify-center gap-[84px] max-w-[1200px] m-auto pc:block pc: w-full pc: mx-auto">
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-two.jpeg" alt="">
+          </div>
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-two2.jpeg" alt="">
+          </div>
+        </div>
+        <div class="py-[30px] text-center">
+          <router-link to="/gallery" class="text-white bg-black pt-[13px] pb-[13px] pr-[20px] pl-[20px] rounded-[10px]  hover:bg-[#474747] duration-300 cursor-pointer">
+            Смотреть еще фото
+          </router-link>
+        </div>
+      </template>
+      <template #tab-br3>
+        <p class="text-black text-[24px] max-w-[1430px] text-center font-light py-[15px] mx-auto">Благодаря наличию собственного туристского оборудования мы организуем походы и водные сплавы различной продолжительности и сложности:
+          Многодневные походы и водные сплавы по Южному Уралу ( реки Белая, Инзер, Зелим, Ай, Юрюзань, озеро Тургаяк, Зюраткуль, национальный парк «Таганай»,
+          Мурадымовское ущелье и др.
+          Однодневные водные сплавы по р. Буй.</p>
+        <div class="flex justify-center gap-[84px] max-w-[1200px] m-auto pc:block pc: w-full pc: mx-auto">
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-three.jpeg" alt="">
+          </div>
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-three2.jpeg" alt="">
+          </div>
+        </div>
+        <div class="py-[30px] text-center">
+          <router-link to="/gallery" class="text-white bg-black pt-[13px] pb-[13px] pr-[20px] pl-[20px] rounded-[10px]  hover:bg-[#474747] duration-300 cursor-pointer">
+            Смотреть еще фото
+          </router-link>
+        </div>
+      </template>
+      <template #tab-br4>
+        <p class="text-black text-[24px] max-w-[1430px] text-center font-light py-[15px] mx-auto">Оказываем информационную, организационную помощь ветеранам спорта в организации поездок на соревнования, организации физкультурно-оздоровительных занятий.</p>
+        <div class="flex justify-center gap-[84px] max-w-[1200px] m-auto pc:block pc: w-full">
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-four.jpeg" alt="">
+          </div>
+          <div>
+            <img class="max-w-[600px] max-h-[400px] rounded-[30px] pc:mx-auto pc:pt-[20px] pc:w-full pc:h-full tablet:rounded-0" src="../assets/tabs-four2.jpeg" alt="">
+          </div>
+        </div>
+        <div class="py-[30px] text-center">
+          <router-link to="/gallery" class="text-white bg-black pt-[13px] pb-[13px] pr-[20px] pl-[20px] rounded-[10px]  hover:bg-[#474747] duration-300 cursor-pointer">
+            Смотреть еще фото
+          </router-link>
+        </div>
+      </template>
+      <template #tab-br5>
+        <p class="text-black text-[24px] max-w-[1430px] text-center font-light py-[15px] mx-auto">ЖДЕМС</p>
+      </template>
+    </tabs>
+  </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
 
 </style>
