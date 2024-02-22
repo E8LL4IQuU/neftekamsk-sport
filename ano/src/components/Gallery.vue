b98db201fb5054c3cf5bf5fef767019bba022602
E8LL4IQuU <ayu@danwin1210.de>
2024-02-22 13:53:22 +0100

    Gallery.vue: remove excessive rows from grid

--- ano/src/components/Gallery.vue
+++ ano/src/components/Gallery.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="grid gap-5 grid-rows-3 grid-flow-col pc:block mobile:gap-2">
-    <div class="row-span-3 pt-[20%] pc:pt-2">
-      <img class="rounded-[30px] w-[495px] h-[495px] pc:m-auto w-[700px] tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]" :src="props.GalleryData.linkImgOne">
+  <div class="lg:grid lg:grid-flow-col gap-5 mobile:gap-2">
+    <div class="pt-[20%] pc:pt-2">
+      <img
+        class="rounded-[30px] h-[495px] pc:m-auto w-[700px] tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]"
+        :src="props.GalleryData.linkImgOne">
       <div class="pt-4 pc:pt-2 ">
-        <img class="rounded-[30px] w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]" :src="props.GalleryData.linkImgTwo">
+        <img
+          class="rounded-[30px] w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]"
+          :src="props.GalleryData.linkImgTwo">
       </div>
     </div>
-    <div class="col-span-2 pc:pt-2">
-      <img class="rounded-[30px] w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]" :src="props.GalleryData.linkImgThree">
-    </div>
-    <div class="pc:pt-2 ">
-      <video class=" w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]" :src="props.GalleryData.videos" controls autoplay muted="true"></video>
+    <div class=" pc:pt-2">
+      <img
+        class="rounded-[30px] w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]"
+        :src="props.GalleryData.linkImgThree">
+      <video class=" w-[700px] h-[426px] pc:m-auto tablet:w-[500px] tablet:h-[313px] mobile:w-[83%]  mobile:h-98vh]"
+        :src="props.GalleryData.videos" controls autoplay muted="true"></video>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-interface IGallery{
+interface IGallery {
   // FIXME: how about link[]
   linkImgOne: string;
   linkImgTwo: string;
@@ -28,6 +33,4 @@ const props = defineProps<{
 }>()
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
