2d565aa0a034207477cfd56dd63050edf1b65606
E8LL4IQuU <ayu@danwin1210.de>
2024-03-02 14:46:53 +0100

    News.vue: make the whole block a link

--- ano/src/components/News.vue
+++ ano/src/components/News.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="pt-5" v-for="(news, index) in props.NewsData" :key="index">
-    <!-- FIXME: the whole block shold probably be a link -->
-    <!-- FIXME: mobile layout: too big y-gap between news -->
-    <div class="w-[406px] h-[445px] hover:-translate-y-5 duration-300 mobile:w-[300px]">
-      <router-link  :to="`/news/${news.ID}`">
+    <!-- FIXME: pointing cursor on bottom of div creates jittery movement -->
+    <router-link :to="`/news/${news.ID}`">
+      <div class="hover:-translate-y-5 duration-300 py-3 md:p-6">
         <img class="w-[350px] h-[260px] object-cover" :src='`${url}/uploads/${news.ImagePath}`'>
-      </router-link>
-      <div class="w-[350px] mobile:w-[300px]">
-        <div class="flex items-center justify-between">
-        <h5 class="text-black font-semibold text-[24px] line-clamp-2">{{news.Title}}</h5>
-        <span class="text-gray-400 font-normal text-[20px]"><span>{{ formatTimestamp(news.CreatedAt) }}</span></span>
+        <div class="w-[350px]">
+          <div class="flex items-center justify-between">
+            <h5 class="text-black font-semibold text-[24px] line-clamp-2">{{ news.Title }}</h5>
+            <span class="text-gray-400 font-normal text-[20px]"><span>{{ formatTimestamp(news.CreatedAt)
+                }}</span></span>
+          </div>
+          <p class="text-gray-500 font-medium text-[20px] line-clamp-3">{{ news.Description }}</p>
+        </div>
       </div>
-        <p class="text-gray-500 font-medium text-[20px] line-clamp-3">{{news.Description}}</p>
-      </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
@@ -26,27 +26,23 @@ const props = defineProps<{
 }>()
 
 const formatTimestamp = (timestamp: number): string => {
-    const date = new Date(timestamp * 1000);
+  const date = new Date(timestamp * 1000);
 
-    // Get the time zone offset in minutes
-    const timeZoneOffset = date.getTimezoneOffset();
-    
-    // Use the offset to create a new date with the local time zone
-    const localDate = new Date(date.getTime() - timeZoneOffset * 60 * 1000);
+  // Get the time zone offset in minutes
+  const timeZoneOffset = date.getTimezoneOffset();
 
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: '2-digit',
-        day: 'numeric',
-    };
+  // Use the offset to create a new date with the local time zone
+  const localDate = new Date(date.getTime() - timeZoneOffset * 60 * 1000);
 
-    return localDate.toLocaleString('ru-RU', options);
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: 'numeric',
+  };
+
+  return localDate.toLocaleString('ru-RU', options);
 };
-
-
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
