4dcd1c9f43727bf5c464c2e5243c60e62c1c1f5c
E8LL4IQuU <ayu@danwin1210.de>
2024-03-02 14:16:19 +0100

    Sidebar.vue: change icon to solid on active

--- ano/src/components/Manage/Sidebar.vue
+++ ano/src/components/Manage/Sidebar.vue
@@ -12,7 +12,9 @@ const props = defineProps(['sidebarData'])
       <ul>
         <li class="flex flex-col">
           <router-link v-for="item in props.sidebarData" :to="item.path"
-            :class="{ 'text-black font-bold': $route.path === item.path, 'text-gray-700': $route.path !== item.path }">{{ item.name }}</router-link>
+            :class="{ 'text-black font-bold': $route.path === item.path, 'text-gray-700': $route.path !== item.path }">
+            {{ item.name }}
+          </router-link>
         </li>
       </ul>
       <!-- TODO: add logout button
@@ -21,15 +23,27 @@ const props = defineProps(['sidebarData'])
 
     <!-- Mobile -->
     <nav class="lg:hidden fixed bottom-0 w-full bg-stone-300 py-3 z-10">
-      <ul class="grid grid-cols-4 text-center justify-between gap-x-3">
-          <router-link v-for="item in props.sidebarData" class="h-12 rounded-lg flex flex-col justify-between items-center" :to="item.path"
+      <ul class="grid grid-cols-4 text-center justify-between">
+        <div v-for="item in props.sidebarData">
+          <router-link v-if="$route.path === item.path" class="h-12 rounded-lg flex flex-col justify-between items-center"
+            :to="item.path"
+            :class="{ 'text-black': $route.path === item.path, 'text-gray-700': $route.path !== item.path }">
+            <div>
+              <component :class="{ 'bg-orange-300 saturate-50 rounded-2xl': $route.path === item.path }" class="h-7 w-16"
+                :is="item.iconActive"></component>
+            </div>
+            <span class="font-bold text-sm">{{ item.name }}</span>
+          </router-link>
+
+          <router-link v-else class="h-12 rounded-lg flex flex-col justify-between items-center" :to="item.path"
             :class="{ 'text-black': $route.path === item.path, 'text-gray-700': $route.path !== item.path }">
             <div>
               <component :class="{ 'bg-orange-300 saturate-50 rounded-2xl': $route.path === item.path }" class="h-7 w-16"
                 :is="item.icon"></component>
             </div>
-            <span class="font-bold text-xs">{{ item.name }}</span>
+            <span class="font-bold text-sm">{{ item.name }}</span>
           </router-link>
+        </div>
       </ul>
       <!-- TODO: add logout button
       POST /api/auth/logout -->
