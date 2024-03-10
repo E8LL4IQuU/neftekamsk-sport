5f7365f31789eeba406b11beede35f9bb3be7545
1Dmitry2 <pain2284@gmail.com>
2024-03-10 11:42:28 +0500

    ЗАнятия жес

--- ano/src/views/ScheduleView.vue
+++ ano/src/views/ScheduleView.vue
@@ -3,7 +3,69 @@
 </script>
 
 <template>
-    <h2>Schedule view</h2>
+    <div class="max-w-[1650px]  m-auto mb-8 ">
+      <h2 class="text-black pt-[14px] pb-[18px] font-light text-4xl mobile:text-3xl">Расписание</h2>
+      <p class="text-[24px] text-[#AFAFAF] font-light">Для записи на занятие, используйте наш WhatsApp <a href="tel:+79173522080" class="text-[#585858] underline decoration-1"> +7 (917) 352 20 80</a></p>
+      <div class="max-w-[1450px]  m-auto mb-8 ">
+        <div class="flex pt-[34px] items-center gap-[2px]">
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px]">Название</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Пн</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Вт</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Ср</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Чт</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Пт</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Сб</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">Вс</p>
+          </div>
+        </div>
+        <div class="flex pt-[2px]  gap-[2px]">
+          <div class="bg-[#B5B5B5] ">
+            <p class="text-white font-bold text-[20px] p-[14px]">Занятие 1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">
+              1 - 3ч
+              <br>
+              обед
+              12:00 - 13:00
+            </p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+          <div class="bg-[#B5B5B5]">
+            <p class="text-white font-bold text-[20px] p-[14px] w-[165px] pl-[9px]">1</p>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <style>
