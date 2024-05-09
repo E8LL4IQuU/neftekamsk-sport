d367b85e60e56781e077a84f3a2727da936de8e3
E8LL4IQuU <ayu@danwin1210.de>
2024-05-09 09:01:48 +0200

    Slider: change styles

--- ano/src/components/Slider.vue
+++ ano/src/components/Slider.vue
@@ -9,10 +9,14 @@
           <img class="w-auto h-72 lg:h-screen object-cover lg:max-h-[700px] brightness-[60%]"
             :src="`${url}/uploads/${slide.ImagePath}`" alt="slider image" />
           <div class="w-full text-white absolute text-center justify-center">
-            <h2 class="text-white text-5xl font-bold pb-12 mobile:text-3xl line-clamp-1">{{ slide.Title }}</h2>
+            <h2 class="text-white text-2xl lg:text-7xl font-bold pb-4 lg:pb-10 line-clamp-1">{{ slide.Title }}</h2>
             <!-- <h3 class="text-white font-normal pb-3 text-[20px]">{{ formatTimestamp(slide.Date) }}</h3> -->
-            <router-link disabled class="py-2 px-4 text-sm rounded-[4px] bg-gray-400 hover:bg-gray-600 duration-300"
-              :to="`/events/${slide.ID}`">Подробнее</router-link>
+            <router-link :to="`/events/${slide.ID}`" class="">
+              <a
+                class="py-3 px-6 font-bold rounded-lg bg-gray-700 hover:bg-gray-600 hover:scale-110 duration-200 ease-out">
+                Подробнее
+              </a>
+            </router-link>
           </div>
         </div>
       </SwiperSlide>
